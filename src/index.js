fc6ff505dabf08befd64c315c480030e9c1f7164
Nithin K P <nithinkp0212@gmail.com>
2025-11-05 22:52:59 +0530

    Refactor Greeting component to include Message components

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import { createRoot } from "react-dom/client";
 function Greeting() {
   return (
     <div>
-      <h2>Welcome to React Learning</h2>
+      <Message />
+      <Message1 />
     </div>
   );
+}
+
+function Message(){
+  return <h1>Hello!, Welcome To React Learning...</h1>
+}
+
+function Message1(){
+  return <p>Let's Begin..!</p>
 }
 // function Greeting(){
 //   return React.createElement("div", {}, React.createElement("h2", {}, "Hello welcome to React Learning Class"))
