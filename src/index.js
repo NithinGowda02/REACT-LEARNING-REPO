d41d62f26901f64eba74cea76069f6c06a718668
Nithin K P <nithinkp0212@gmail.com>
2025-11-05 22:48:07 +0530

    Refactor Greeting component to use JSX syntax

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-// function Greeting() {
-//   return (
-//     <div>
-//       <h2>Welcome to React Learning</h2>
-//     </div>
-//   );
-// }
-function Greeting(){
-  return React.createElement("div", {}, React.createElement("h2", {}, "Hello welcome to React Learning Class"))
+function Greeting() {
+  return (
+    <div>
+      <h2>Welcome to React Learning</h2>
+    </div>
+  );
 }
+// function Greeting(){
+//   return React.createElement("div", {}, React.createElement("h2", {}, "Hello welcome to React Learning Class"))
+// }
 
 const container = document.getElementById('root');
 const roots = createRoot(container);
