b0197fa895184717e08f90ff1c4f073c66119864
Nithin K P <nithinkp0212@gmail.com>
2025-11-05 22:49:37 +0530

    Adding Nesting of Components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-// function Greeting() {
-//   return (
-//     <div>
-//       <h2>Welcome to React Learning</h2>
-//     </div>
-//   );
-// }
-function Greeting(){
-  return React.createElement("div", {}, React.createElement("h2", {}, "Hello welcome to React Learning Class"))
+function Greeting() {
+  return (
+    <div>
+      <Message />
+      <Message1 />
+    </div>
+  );
 }
+
+function Message(){
+  return <h1>Hello!, Welcome To React Learning...</h1>
+}
+
+function Message1(){
+  return <p>Let's Begin..!</p>
+}
+// function Greeting(){
+//   return React.createElement("div", {}, React.createElement("h2", {}, "Hello welcome to React Learning Class"))
+// }
 
 const container = document.getElementById('root');
 const roots = createRoot(container);
