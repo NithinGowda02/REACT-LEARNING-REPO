4d84a7704ca7e5c875804215d803ef4ad06ba37c
Nithin K P <nithinkp0212@gmail.com>
2025-11-06 11:54:01 +0530

    Resolve merge conflict in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-// function Greeting() {
-//   return (
-//     <div>
-//       <h2>Welcome to React Learning</h2>
-//     </div>
-//   );
-// }
-function Greeting(){
-  return React.createElement("div", {}, React.createElement("h2", {}, "Hello welcome to React Learning Class"))
+import './index.css';
+
+function Greeting() {
+  return (
+    <div>
+      <Message title="Welcome to React" title1="Let's Begin..!"/>
+      
+    </div>
+  );
 }
+
+function Message({ title, title1 }){
+  return (
+  <>  
+  <h1 className="heading">{title}</h1>
+  <p className="paragraph">{title1}</p>
+  </>
+);
+}
+// function Greeting(){
+//   return React.createElement("div", {}, React.createElement("h2", {}, "Hello welcome to React Learning Class"))
+// }
 
 const container = document.getElementById('root');
 const roots = createRoot(container);
