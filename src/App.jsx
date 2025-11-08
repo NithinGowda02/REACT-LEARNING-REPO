5618ed1bf91da7afc6be62d82b272786b3d0cc49
Nithin K P <nithinkp0212@gmail.com>
2025-11-08 16:54:17 +0530

    My First React Project on E-Commerce

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navigation from './components/nav';
+import Hero_Section from "./components/hero-section";
+
+const App = () =>{
+    return (
+        <>  
+            <Navigation />
+            <Hero_Section />
+        </>
+    );
+}
+
+export default App;
